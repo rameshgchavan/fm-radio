e40d2807d5095cfb78cc5c5d1e9fc6e5869b8b9c
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-10 23:04:12 +0530

    pages added

---
+++ src/pages/Admin.js
@@ -0,0 +1,11 @@
+import Broadcast from '../components/Broadcast'
+
+const Admin = () => {
+    return (
+        <div>
+            <Broadcast />
+        </div>
+    )
+}
+
+export default Admin
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import Listen from '../components/Listen'
+
+const Home = () => {
+    return (
+        <div>
+            <Listen />
+        </div>
+    )
+}
+
+export default Home
