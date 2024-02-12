2109027e4325dee876c39979ad7f02b8755b02d0
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-13 03:33:45 +0530

    build update: log updated

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const io = new Server(server, { cors: true });
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
+	console.log("connection estalished by id ", socket.id)
 
 	socket.on("callUser", (data) => {
 		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from })
