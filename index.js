f939cf67693e985708b72ec9ba702dc696de045d
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-13 05:05:21 +0530

    buil updated added or path

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: true });
 
-console.log("io ", io)
-
 io.on("connection", (socket) => {
-	socket.emit("me", socket.id)
 	console.log("connection estalished by id ", socket.id)
+	socket.emit("me", socket.id)
+	
 
 	socket.on("callUser", (data) => {
 		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from })
