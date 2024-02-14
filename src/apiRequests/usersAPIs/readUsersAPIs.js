f79128100a87843491c126d79d0c65e269a2c627
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-15 04:20:02 +0530

    api request added

---
+++ src/apiRequests/usersAPIs/readUsersAPIs.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+// Get a user
+const readUserRequest = async (crediantials) => {
+    return (
+        (await axios(`/users/login`,
+            {
+                method: "post",
+                data: crediantials
+            }
+        ))?.data
+    );
+};
+
+const readBroadcastIdRequest = async (email) => {
+    return (
+        (await axios(`/users/id`,
+            {
+                method: "get",
+                headers: { email }
+            }
+        ))?.data
+    );
+};
+
+// Check user's Email ID
+const readUserEmailRequest = async (Email) => {
+    return (
+        (await axios(`/users/isemail`,
+            {
+                method: "post",
+                data: Email
+            }
+        ))?.data
+    );
+};
+
+export {
+    readUserRequest,
+    readBroadcastIdRequest,
+    readUserEmailRequest
+}
