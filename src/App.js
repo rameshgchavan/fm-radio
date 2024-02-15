4f60694c87fda394006bac273e5abc71fc4771e5
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-15 21:44:21 +0530

    app css added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import "./App.css"
 import pageRoutes from "./routes/pageRoutes"
 
 function App() {
-  // const scrutinizedUser = {
-  //   token: "token"
-  // };
-
   const { scrutinizedUser } = useSelector(state => state.usersReducer);
 
   return (
-    <div >
+    <div className="App">
       {pageRoutes(scrutinizedUser)}
     </div>
   )
