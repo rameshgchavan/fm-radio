57ee17d0221bddc8f1378916527eca9cd335036f
Ramesh_Chavan <ramesh7452@gmail.com>
2024-02-17 20:58:12 +0530

    extra fuctions removed

--- src/redux/features/usersSlice.js
+++ src/redux/features/usersSlice.js
@@ -3,9 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // initialized state
 const initialState =
 {
-    users: [],
-    scrutinizedUser: {},
-    userStatus: "approved"
+    scrutinizedUser: {}
 };
 
 const usersSlice = createSlice({
@@ -13,32 +11,18 @@ const usersSlice = createSlice({
     initialState,
     reducers:
     {
-        // setting data in users array
-        addUsersAction: (state, action) => {
-            // overriding users and spreading payload array
-            state.users = [...action.payload];
-        },
-        // adding new user in users array
-        addUserAction: (state, action) => {
-            state.users.push(action.payload);
-        },
         // setting data in scrutinizedUser object 
         addScrutinizedUserAction: (state, action) => {
             // overriding scrutinizedUser
             state.scrutinizedUser = action.payload;
-        },
-        addUserStatus: (state, action) => {
-            state.userStatus = action.payload;
         }
     }
 });
 
 // exporting actions
 export const {
-    addUsersAction,
-    addUserAction,
     addScrutinizedUserAction,
-    addUserStatus
 } = usersSlice.actions;
+
 // exporting reducer
 export default usersSlice.reducer;
